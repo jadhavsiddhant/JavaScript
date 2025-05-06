265c656e8339b0f67368a45c8282d71e98412bbc
Siddhant Jadhav <siddhantjadhav0106@gmail.com>
2025-05-06 21:07:05 +0530

    Create String.js

---
+++ Js-Basics/String.js
@@ -0,0 +1,11 @@
+let str = "this is \"string\""
+console.log(str)
+console.log(str.length)
+console.log()
+ 
+let s = "hello javascript"
+console.log(s.at(4))
+console.log(s.charCodeAt(0))
+console.log(s.indexOf("llo"))
+console.log(s.substring(0,-5))
+console.log(s.slice(0,-5))
