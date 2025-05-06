f5303ab20bb4820113848a667fbd5e3ef255a1a8
Siddhant Jadhav <siddhantjadhav0106@gmail.com>
2025-05-06 21:08:55 +0530

    Create stringcomparison.js

---
+++ Promblems/stringcomparison.js
@@ -0,0 +1,9 @@
+function stringcomparison(a,b){
+    if(a.length == b.length){
+        console.log('True');
+    }
+    else{
+        console.log('False');
+    }
+}
+stringcomparison('abc','defg')
