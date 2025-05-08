a3cede77dd26206bb96f853b62f861d3eb6cb1f5
Siddhant Jadhav <siddhantjadhav0106@gmail.com>
2025-05-08 11:07:33 +0530

    Create timeconversion.js

---
+++ Promblems/timeconversion.js
@@ -0,0 +1,9 @@
+function timeconvertionintosec(){
+    let min = prompt("Enter the time in minutes");
+    let hours = prompt("Enter the time in hours");
+
+    console.log("Time in seconds is: " + ((min * 60) + (hours * 3600)));
+
+
+}
+timeconvertionintosec();
