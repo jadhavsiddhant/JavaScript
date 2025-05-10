60574993403e5f77c8827512f0f30eefb8eae533
Siddhant Jadhav <siddhantjadhav0106@gmail.com>
2025-05-10 08:06:01 +0530

    Create elements.js

---
+++ Js-Basics/elements.js
@@ -0,0 +1,11 @@
+document.getElementById("myclass")[0].innerHTML = "Helli Sir!"
+document.createElement("div")
+
+document.getElementById("myid").class = "myclass"
+
+function myFunction(){
+    var x = 5;
+    var y = 10;
+    var z = parseInt(x)+parseInt(y);
+    document.getElementById("chapter1").innerHTML = z;
+}
