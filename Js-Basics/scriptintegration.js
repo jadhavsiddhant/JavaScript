6b18ecbaa8a697d5832579509df942c617136bb6
Siddhant Jadhav <siddhantjadhav0106@gmail.com>
2025-05-11 08:54:38 +0530

    Create scriptintegration.js

---
+++ Js-Basics/scriptintegration.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Form + Script</title>
+</head>
+<body>
+  <form id="form">
+    <input type="text" id="name" placeholder="Enter your name" />
+    <button type="submit">Submit</button>
+  </form>
+
+  <script src="form.js"></script>
+</body>
+</html>
