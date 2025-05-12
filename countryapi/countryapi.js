8f0ef1cbe6d8ac646719a5fa01ba1b5aa6a64fa4
Siddhant Jadhav <siddhantjadhav0106@gmail.com>
2025-05-12 22:37:47 +0530

    Create countryapi.js

---
+++ countryapi/countryapi.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Country API app</title>
+</head>
+<body>
+    <h1> Country</h1>
+    <div>
+        <input type = "text" id = "userInput"> 
+        <button onclick="handlesearch()"> search</button>
+
+    </div>
+    <div id = "result">
+        
+
+    </div>
+
+    <script src = "script.js"></script>
+</body>
+</html>
