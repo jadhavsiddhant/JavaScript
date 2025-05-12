c16e402bdb2d644d314f0789dd576ff0c4c4bfa7
Siddhant Jadhav <siddhantjadhav0106@gmail.com>
2025-05-12 22:36:09 +0530

    Create gitsearch.js

---
+++ gitsearch/gitsearch.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Github Search</title>
+</head>
+<body>
+    <div>
+        <h1>Github Profile Search</h1>
+        <label for="username">Enter your github profile: </label>
+        <input type="text" name="user" id="username">
+        <br>
+        <button onclick="handleSearch()">Search</button>
+    </div>
+</body>
+<script src="script.js"></script>
+</html>
